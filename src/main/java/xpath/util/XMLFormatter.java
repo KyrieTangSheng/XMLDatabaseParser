2def2e36d433dba7cb50144528a366b7708c0d94
KyrieTangSheng <tangsheng001018@gmail.com>
2025-02-04 22:05:09 -0800

    better formatting of output

--- src/main/java/xpath/util/XMLFormatter.java
+++ src/main/java/xpath/util/XMLFormatter.java
@@ -2,9 +2,21 @@ package xpath.util;
 
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
+import java.util.HashSet;
+import java.util.Arrays;
+import java.util.Set;
 
 public class XMLFormatter {
+    // Common inline elements that shouldn't create new lines
+    private static final Set<String> INLINE_ELEMENTS = new HashSet<>(Arrays.asList(
+        "em", "strong", "a", "span", "code", "i", "b", "sub", "sup"
+    ));
+    
     public static String formatNode(Node node) {
+        return formatNode(node, 0);
+    }
+
+    private static String formatNode(Node node, int indent) {
         StringBuilder sb = new StringBuilder();
         String nodeName = node.getNodeName();
 
@@ -14,6 +26,13 @@ public class XMLFormatter {
             return text.isEmpty() ? "" : text;
         }
 
+        boolean isInline = INLINE_ELEMENTS.contains(nodeName.toLowerCase());
+        
+        // Add newline and indentation for block elements
+        if (!isInline) {
+            sb.append("\n").append("    ".repeat(indent));
+        }
+
         // Start tag
         sb.append("<").append(nodeName);
         
@@ -21,7 +40,8 @@ public class XMLFormatter {
         if (node.hasAttributes()) {
             for (int i = 0; i < node.getAttributes().getLength(); i++) {
                 Node attr = node.getAttributes().item(i);
-                sb.append(" ").append(attr.getNodeName()).append("=\"").append(attr.getNodeValue().trim()).append("\"");
+                sb.append(" ").append(attr.getNodeName())
+                  .append("=\"").append(attr.getNodeValue().trim()).append("\"");
             }
         }
         sb.append(">");
@@ -36,12 +56,21 @@ public class XMLFormatter {
                 sb.append(textContent);
             }
         } else {
+            boolean hasNonTextChildren = false;
             for (int i = 0; i < children.getLength(); i++) {
                 Node child = children.item(i);
-                String childText = formatNode(child);
+                if (!child.getNodeName().equals("#text")) {
+                    hasNonTextChildren = true;
+                }
+                String childText = formatNode(child, indent + 1);
                 if (!childText.isEmpty()) {
                     sb.append(childText);
                 }
+            }
+            
+            // Add newline and indent for closing tag of block elements with children
+            if (!isInline && hasNonTextChildren) {
+                sb.append("\n").append("    ".repeat(indent));
             }
         }
 
